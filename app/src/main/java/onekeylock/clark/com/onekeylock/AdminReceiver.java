dfa3d318cc9b3237281e2698dccc2039fbc55e8b
Clark <zhaody_frt@163.com>
2016-05-31 14:56:34 +0800

    提交一键锁屏代码

---
+++ app/src/main/java/onekeylock/clark/com/onekeylock/AdminReceiver.java
@@ -0,0 +1,10 @@
+package onekeylock.clark.com.onekeylock;
+
+/**
+ * Created by clark on 2015/7/9.
+ */
+
+import android.app.admin.DeviceAdminReceiver;
+
+public class AdminReceiver extends DeviceAdminReceiver {
+}
